f634a71348287b2caa111e8734eb72e3c18f12d2
saksham-tomer <sakshamtomerdevs@gmail.com>
2024-03-21 21:57:22 +0530

    added

--- practice.sol
+++ practice.sol
@@ -333,9 +333,34 @@ contract sendToFallback{
     }
 }
 
+contract receiver{
+    event Received(address caller, uint amount, string message);
+
+    fallback() external payable {
+        emit Received(msg.sender, msg.value, "fallback was called");
+    }
+    function foo(string memeory  _message,uint _x )public payable returns(uint){
+        emit Received(msg.sender, msg.value,_message);
+
+        return _x +1;
+    }
+}
 
 
+contract caller{
+    event Response(bool success, bytes data);
 
+    //lets imagine that the source caller doesnt have the souce code for the contract Receiver, but we dont know the address of contract receiver and the function to call
+
+    function testCallFoo(address payable _addr)public payable{
+        //you can send ether and specify a custom gas amount 
+
+        (bool success, bytes memory data) = _addr.call{value: msg.value, gas: 5000}(abi.encodeWithSignature("foo(string,uint256)", "call foo",123));
+
+
+        emit Response(success,data);
+    }
+}
 
 
 
